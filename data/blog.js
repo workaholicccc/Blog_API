7cd1fd623c1d0d5e3499d6b7e3239984049c579b
workaholicccc <hossain.kifayat100@gmail.com>
2026-09-10 01:15:22 +0600

    working on the data file

--- data/blog.js
+++ data/blog.js
@@ -1,7 +1,40 @@
 //this file will be used to store the data for this project
 
-let posts=[]
+const {v4:uuidv4}=require('uuid')   //needed for generating unique IDs for each post
 
-const BlogData={
+let posts=[]  //will store the data
 
+
+//creating a function to generate new posts
+function createPost(postData){   
+    const newPost={      //tells us what a post will contain 
+        id:uuidv4(),
+        title:postData.title,
+        content:postData.content,
+        author:postData.author || 'Anonymous',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+    }
+
+    posts.push(newPost)  //storing 'newPost' in posts
+
+    return newPost
+}
+
+
+//getting all the posts
+function findAllPosts(){
+    return posts
+}
+
+
+//getting a post by ID
+function findPostByID(id){
+    return posts.find(post=>post.id===id)
+}
+
+
+//updating a post
+function updatePost(id){
+    
 }
